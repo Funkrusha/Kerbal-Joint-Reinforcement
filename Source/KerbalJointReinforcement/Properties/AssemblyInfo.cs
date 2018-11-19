7b1a31cda3ef8a1864f482e10aa9d888532b6b15
Lisias T <me@lisias.net>
2018-11-19 00:17:49 -0200

    Marking this branch EXPERIMENTAL

--- Source/KerbalJointReinforcement/Properties/AssemblyInfo.cs
+++ Source/KerbalJointReinforcement/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KerbalJointReinforcement")]
+[assembly: AssemblyTitle("KerbalJointReinforcement /L Experimental")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
